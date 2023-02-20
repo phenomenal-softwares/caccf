9543086fa1ee17f6b358ce7dd72bf07557fd2093
Phenomenal <115654413+phenomenal-softwares@users.noreply.github.com>
2023-02-20 11:57:56 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,169 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When was the Power Land established?",
+    answer: "2021",
+    options: [
+      "2019",
+      "2020",
+      "2021",
+      "2009"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Who was the president during Power Land's establishment?",
+    answer: "None of the above",
+    options: [
+      "Onifade Abigail",
+      "Bola Ahmed Tinubu",
+      "Elusoji Feyikemi",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who took prayer session on CACCF Drama Night?",
+    answer: "Bro Blessing",
+    options: [
+      "Sis Lolade",
+      "Bro Blessing",
+      "Bro Joshua",
+      "Sis Abigail"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What's the Greek word for prayer?",
+    answer: "Proseuche",
+    options: [
+      "Lambano",
+      "Alhamdulillah",
+      "Prayoques",
+      "Proseuche"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of these men prayed while his face was between his knees ?",
+    answer: "Elijah",
+    options: [
+      "Elisha",
+      "Jesus",
+      "Onewo Theophilus",
+      "Elijah"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What time does prayer meeting start on Monday?",
+    answer: "17:00",
+    options: [
+      "5:30pm",
+      "17:00",
+      "5:15pm",
+      "16:00"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What's the prayer unit's tenure theme?",
+    answer: "None of the above",
+    options: [
+      "Rekindling",
+      "Rekindling the fire of dead pulpit",
+      "Renewing the fire on dead altars",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 8,
+    question: "The Greek word for Bible is?",
+    answer: "Biblos",
+    options: [
+      "Biblos",
+      "Bibliah",
+      "Biblios",
+      "Biblah"
+    ]
+  },
+    {
+    numb: 9,
+    question: "The Hebrew word 'Chaya' means ___",
+    answer: "To be quickened",
+    options: [
+      "Refinary",
+      "To be quickened",
+      "To grow",
+      "To love"
+    ]
+  },
+    {
+    numb: 10,
+    question: "What is the name of the 2021 set Prayer Cord?",
+    answer: "Oyesiji Blessing",
+    options: [
+      "Adeniji Zacchaeus",
+      "Sister Ifejesu",
+      "Elusoji Feyikemi",
+      "Oyesiji Blessing"
+    ]
+  },
+{
+    numb: 11,
+    question: "Which of these scripture pertains to prayer?",
+    answer: "Psalm 66:19",
+    options: [
+      "Psalm 66:19",
+      "Luke 19:1",
+      "John 6:2",
+      "Revelations 1:8"
+    ]
+  },
+{
+    numb: 12,
+    question: "Who made the statement 'asiri ori pepe o kin tu'?",
+    answer: "Bro Joshua",
+    options: [
+      "Bro Blessing",
+      "Pastor Samuel",
+      "Bro Joshua",
+      "Sam Ogidiolu"
+    ]
+  },
+{
+    numb: 13,
+    question: "What's the name of the present Prayer Cord?",
+    answer: "Oyedamola",
+    options: [
+      "Oyetem",
+      "Oyedamole",
+      "Oyedamope",
+      "Oyedamola"
+    ]
+  },
+{
+    numb: 14,
+    question: "God speaks to whom by the river Cheba?",
+    answer: "Ezekiel",
+    options: [
+      "Ezekiel",
+      "Isaiah",
+      "Moses",
+      "Joshua"
+    ]
+  },
+{
+    numb: 15,
+    question: "In 1Corinthians 16: Anathema Maranatha means ",
+    answer: "Accursed",
+    options: [
+      "Abuse",
+      "Accursed",
+      "Go thee behind Satan",
+      "God my strength"
+    ]
+  },
+
+];
